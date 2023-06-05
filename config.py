71836ae5459de2636069b37cc9064789495eb081
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2023-06-05 23:11:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001821513964)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001821513964")
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "༒ʙᴀʙʏ qυєєи༒")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5613961045").split()))
